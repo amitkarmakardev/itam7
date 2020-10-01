f01900d4cd1aff1866c0f1932964c8bf2e002973
Amit Karmakar <amitkarmakar.dev@gmail.com>
2020-10-01 13:48:36 +0530

    Update pending tasks page

--- resources/views/task/pending.blade.php
+++ resources/views/task/pending.blade.php
@@ -6,66 +6,66 @@
 
 
 @section('subcontent')
-    <section class="content-header">
-        <h1>{{ ucwords($type) }} task list <a href="{{ url('task', ['create']) }}">(+)</a></h1>
-    </section>
+<section class="content-header">
+    <h1>{{ ucwords($type) }} task list <a href="{{ url('task', ['create']) }}">(+)</a></h1>
+</section>
 
-    <section class="content">
-        <div class="box box-primary">
-            <div class="box-body">
+<section class="content">
+    <div class="box box-primary">
+        <div class="box-body">
             <h4>Today's Task</h4>
             <table class="table table-striped">
-                    <thead>
+                <thead>
                     <tr>
-                        <th style="width: 15%">Asset ID</th>
                         <th style="width: 50%">Task</th>
+                        <th style="width: 15%">Asset ID</th>
                         <th style="width: 10%">Created</th>
                         <th style="width: 10%">Updated</th>
                         <th style="width: 15%" class="text-center">Complete</th>
                     </tr>
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
                     @foreach($today as $task)
-                        <tr>
-                                <td><a href="{{ url('device', ['view',  $task->asset_id])}}" target="_blank" >{{ $task->asset_id }}</a> </td>
-                            <td>
-                                <a href="{{ url('task', ['view', $task->id]) }}" @if($task->isNew()) class="new" @endif>
-                                    {{ $task->description }}
-                                </a>
-                                <br>
-                                @if($task->latestProgress() != null)
-                                    <small class="text text-warning">> {{ $task->latestProgress()->description }}</small>
-                                @endif
+                    <tr>
+                        <td>
+                            <a href="{{ url('task', ['view', $task->id]) }}" @if($task->isNew()) class="new" @endif>
+                                {{ $task->description }}
+                            </a>
+                            <br>
+                            @if($task->latestProgress() != null)
+                            <small class="text text-warning">> {{ $task->latestProgress()->description }}</small>
+                            @endif
 
-                            </td>
-                            <td>{{ $task->created_at->format('d/m/Y') }}</td>
-                            <td>   
-                                @if($task->latestProgress() != null)
-                                    {{ $task->latestProgress()->created_at->diffForHumans() }} 
-                                @endif
-                            </td>
-                            <td class="text-center">
-                                @if($task->completed_at == null)
-                                <a href="{{ url('task', ['complete', $task->id])}}"
-                                    onclick="return confirm('Are you sure?')">
-                                    <i class="fa fa-check-circle-o" style="font-size: 1.2em; color: green"></i>
-                                </a>
-                                @else
-                                {{ $task->completed_at->format('d/m/Y') }}
-                                @endif
-                            </td>
-                        </tr>
+                        </td>
+                        <td><a href="{{ url('device', ['view',  $task->asset_id])}}" target="_blank">{{ $task->asset_id }}</a> </td>
+
+                        <td>{{ $task->created_at->format('d/m/Y') }}</td>
+                        <td>
+                            @if($task->latestProgress() != null)
+                            {{ $task->latestProgress()->created_at->diffForHumans() }}
+                            @endif
+                        </td>
+                        <td class="text-center">
+                            @if($task->completed_at == null)
+                            <a href="{{ url('task', ['complete', $task->id])}}" onclick="return confirm('Are you sure?')">
+                                <i class="fa fa-check-circle-o" style="font-size: 1.2em; color: green"></i>
+                            </a>
+                            @else
+                            {{ $task->completed_at->format('d/m/Y') }}
+                            @endif
+                        </td>
+                    </tr>
                     @endforeach
-                    </tbody>
-                </table>
-                <br>
-            </div>
+                </tbody>
+            </table>
+            <br>
         </div>
-        <div class="box box-primary">
-            <div class="box-body">
-                <h4>Old</h4>
-                <table class="table table-striped" id="index">
-                    <thead>
+    </div>
+    <div class="box box-primary">
+        <div class="box-body">
+            <h4>Old</h4>
+            <table class="table table-striped" id="index">
+                <thead>
                     <tr>
                         <th style="width: 15%">Asset ID</th>
                         <th style="width: 50%">Task</th>
@@ -73,43 +73,42 @@
                         <th style="width: 10%">Updated</th>
                         <th style="width: 15%" class="text-center">Complete</th>
                     </tr>
-                    </thead>
-                    <tbody>
+                </thead>
+                <tbody>
                     @foreach($tasks as $task)
-                        <tr>
-                                <td><a href="{{ url('device', ['view',  $task->asset_id])}}" target="_blank" >{{ $task->asset_id }}</a> </td>
-                            <td>
-                                <a href="{{ url('task', ['view', $task->id]) }}" @if($task->isNew()) class="new" @endif>
-                                    {{ $task->description }}
-                                </a>
-                                <br>
-                                @if($task->latestProgress() != null)
-                                    <small class="text text-warning">> {{ $task->latestProgress()->description }}</small>
-                                @endif
+                    <tr>
+                        <td><a href="{{ url('device', ['view',  $task->asset_id])}}" target="_blank">{{ $task->asset_id }}</a> </td>
+                        <td>
+                            <a href="{{ url('task', ['view', $task->id]) }}" @if($task->isNew()) class="new" @endif>
+                                {{ $task->description }}
+                            </a>
+                            <br>
+                            @if($task->latestProgress() != null)
+                            <small class="text text-warning">> {{ $task->latestProgress()->description }}</small>
+                            @endif
 
-                            </td>
-                            <td>{{ $task->created_at->format('d/m/Y') }}</td>
-                            <td>   
-                                @if($task->latestProgress() != null)
-                                    {{ $task->latestProgress()->created_at->diffForHumans() }} 
-                                @endif
-                            </td>
-                            <td class="text-center">
-                                @if($task->completed_at == null)
-                                <a href="{{ url('task', ['complete', $task->id])}}"
-                                   onclick="return confirm('Are you sure?')">
-                                    <i class="fa fa-check-circle-o" style="font-size: 1.2em; color: green"></i>
-                                </a>
-                                @else
-                                {{ $task->completed_at->format('d/m/Y') }}
-                                @endif
-                            </td>
-                        </tr>
+                        </td>
+                        <td>{{ $task->created_at->format('d/m/Y') }}</td>
+                        <td>
+                            @if($task->latestProgress() != null)
+                            {{ $task->latestProgress()->created_at->diffForHumans() }}
+                            @endif
+                        </td>
+                        <td class="text-center">
+                            @if($task->completed_at == null)
+                            <a href="{{ url('task', ['complete', $task->id])}}" onclick="return confirm('Are you sure?')">
+                                <i class="fa fa-check-circle-o" style="font-size: 1.2em; color: green"></i>
+                            </a>
+                            @else
+                            {{ $task->completed_at->format('d/m/Y') }}
+                            @endif
+                        </td>
+                    </tr>
                     @endforeach
-                    </tbody>
-                </table>
-                {!! $tasks->render() !!}
-            </div>
+                </tbody>
+            </table>
+            {!! $tasks->render() !!}
         </div>
-    </section>
+    </div>
+</section>
 @stop
